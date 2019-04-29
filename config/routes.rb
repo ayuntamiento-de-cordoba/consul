50fd51506c57e87121c12b7789a92dbf11e2cc3e
carcamcue <carcamcue@gmail.com>
2019-04-29 14:40:31 +0200

    - Añadida página de Procesos Participativos. - Creado custom Welcome controller.

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,7 @@ Rails.application.routes.draw do
   get 'help',             to: 'pages#show', id: 'help/index',             as: 'help'
   get 'help/how-to-use',  to: 'pages#show', id: 'help/how_to_use/index',  as: 'how_to_use'
   get 'mas-informacion/faq', to: 'pages#show', id: 'help/faq/index',      as: 'faq'
+  get 'proposals_law',    to: 'pages#show', id: 'proposals_law/index',    as: 'proposals_law'
 
   # Static pages
   get '/blog' => redirect("http://blog.consul/")
